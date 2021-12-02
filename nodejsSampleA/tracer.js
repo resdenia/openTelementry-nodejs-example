79edcd33632ee3d5f2dcc0aac49bd1c7c3401aa1
AntonKolomiiets <anton.kolomiiets@logz.io>
2021-12-02 14:42:16 +0200

    update tracer

--- nodejsSampleA/tracer.js
+++ nodejsSampleA/tracer.js
@@ -31,4 +31,17 @@ const sdk = new opentelemetry.NodeSDK({
 	instrumentations: [getNodeAutoInstrumentations()],
 });
 
-sdk.start();
+sdk
+	.start()
+	.then(() => {
+		console.log("Tracing initialized");
+	})
+	.catch((error) => console.log("Error initializing tracing", error));
+
+process.on("SIGTERM", () => {
+	sdk
+		.shutdown()
+		.then(() => console.log("Tracing terminated"))
+		.catch((error) => console.log("Error terminating tracing", error))
+		.finally(() => process.exit(0));
+});
